3ef902f21a8a9ccc085febb9f717f9e9eeef8dc5
Joanna Chądzyńska <jmalek87@gmail.com>
2021-04-09 21:19:49 +0200

    finish the project

--- progress-steps/js/script.js
+++ progress-steps/js/script.js
@@ -1,14 +1,44 @@
-const panels = document.querySelectorAll('.panel');
+const progress = document.getElementById('progress');
+const prev = document.getElementById('pre');
+const next = document.getElementById('next');
+const steps = document.querySelectorAll('.circle');
 
-panels.forEach((panel) =>
-	panel.addEventListener('click', () => {
-		removeActiveClasses();
-		panel.classList.add('active');
-	})
-);
+let currentActive = 1;
 
-function removeActiveClasses() {
-	panels.forEach((panel) => {
-		panel.classList.remove('active');
+next.addEventListener('click', () => {
+	currentActive++;
+	if (currentActive > steps.length) {
+		currentActive = steps.length;
+	}
+	update();
+});
+prev.addEventListener('click', () => {
+	currentActive--;
+	if (currentActive < 1) {
+		currentActive = 1;
+	}
+	update();
+});
+
+function update() {
+	steps.forEach((step, idx) => {
+		if (idx < currentActive) {
+			step.classList.add('active');
+		} else {
+			step.classList.remove('active');
+		}
 	});
+
+	const actives = document.querySelectorAll('.active');
+	progress.style.width =
+		((actives.length - 1) / (steps.length - 1)) * 100 + '%';
+
+	if (currentActive === 1) {
+		prev.disabled = true;
+	} else if (currentActive === steps.length) {
+		next.disabled = true;
+	} else {
+		prev.disabled = false;
+		next.disabled = false;
+	}
 }
