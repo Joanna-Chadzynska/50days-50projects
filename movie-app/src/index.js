857446acb6baba4713d63da5ac59dd9f5a62a550
Joanna Chądzyńska <jmalek87@gmail.com>
2021-04-30 18:40:19 +0200

    update movie app

--- movie-app/src/index.js
+++ movie-app/src/index.js
@@ -5,7 +5,16 @@ import '@/styles/index.scss'
 const header = document.createElement('header')
 const form = document.createElement('form')
 const input = document.createElement('input')
+const main = document.createElement('main')
+const movie = document.createElement('div')
+const movieCover = document.createElement('img')
+const movieInfo = document.createElement('div')
+const movieTitle = document.createElement('h3')
+const movieRating = document.createElement('span')
+const movieOverview = document.createElement('div')
+
 form.setAttribute('id', 'form')
+
 input.setAttribute('type', 'search')
 input.setAttribute('placeholder', 'Search...')
 input.setAttribute('id', 'search')
@@ -14,9 +23,23 @@ input.classList = 'search'
 form.appendChild(input)
 header.appendChild(form)
 
-const main = document.createElement('main')
 main.setAttribute('id', 'main')
+movie.classList = 'movie'
 
+movieCover.src = `https://images.unsplash.com/photo-1518173835740-f5d14111d76a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2077&q=80`
+
+movieInfo.classList = 'movie-info'
+movieTitle.innerText = 'Movie Title'
+movieRating.classList = `green`
+movieRating.innerText = 9.8
+movieInfo.append(movieTitle, movieRating)
+
+movieOverview.classList = 'overview'
+movieOverview.innerHTML = ` <h3>Overview</h3>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et odio purus. In lobortis vestibulum venenatis. Sed commodo elit at cursus laoreet. Curabitur sit amet purus sed mauris lobortis lacinia commodo ac lectus. Fusce viverra massa vitae erat ultrices mattis. Morbi a arcu sed erat venenatis pretium vel at libero.`
+
+movie.append(movieCover, movieInfo, movieOverview)
+main.append(movie)
 const app = document.querySelector('#root')
 
 app.append(header, main)
--- movie-app/src/js/api/api.js
+++ movie-app/src/js/api/api.js
@@ -1,14 +1,14 @@
 export async function getPost(id) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${id}`
-  )
-  const json = await response.json()
+    const response = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${id}`
+    )
+    const json = await response.json()
 
-  return json
+    return json
 }
 
 // Use the API_KEY
-const client = process.env.TMDB_API_KEY
-const URL = `https://api.themoviedb.org/3/movie/550?api_key=3f1f5c28069c7144d7ae13ffdaa9c846`
-const apiKey = process.env.TMDB_API_KEY
-console.log(client)
+// const client = process.env.TMDB_API_KEY
+// const URL = `https://api.themoviedb.org/3/movie/550?api_key=3f1f5c28069c7144d7ae13ffdaa9c846`
+// const apiKey = process.env.TMDB_API_KEY
+// console.log(client)
