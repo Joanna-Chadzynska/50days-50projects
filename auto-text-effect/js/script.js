ffd8335fa409cb90285d030249c6760b42713670
Joanna Chądzyńska <jmalek87@gmail.com>
2021-05-18 20:58:03 +0200

    finish auto text typing effect with speed control

--- auto-text-effect/js/script.js
+++ auto-text-effect/js/script.js
@@ -1,1 +1,22 @@
-console.log('start app');
+const textEl = document.getElementById('text');
+const speedInput = document.getElementById('speed');
+
+const text = 'We Love Programming!';
+
+let speed = 300 / speedInput.value;
+let idx = 1;
+
+writeText();
+
+function writeText() {
+	textEl.innerText = text.slice(0, idx);
+	idx++;
+
+	if (idx > text.length) {
+		idx = 1;
+	}
+
+	setTimeout(writeText, speed);
+}
+
+speedInput.addEventListener('input', (e) => (speed = 300 / e.target.value));
